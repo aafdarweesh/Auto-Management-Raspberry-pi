3851ae17cbe30e8c954a05c8d738629363f462f4
aafdarweesh <aafdarweesh@gmail.com>
2018-11-11 12:45:59 +0300

    Update GitCommand.py

--- GitCommand.py
+++ GitCommand.py
@@ -2,13 +2,16 @@ import subprocess
 import time
 
 print(subprocess.getstatusoutput('pkill -f Auto-Management-Raspberry-pi/Streaming.py'))#Stop the execution of the current program
+#remove the current version of it
+print(subprocess.getstatusoutput('rm Auto-Management-Raspberry-pi/*'))
+print(subprocess.getstatusoutput('rm -rf Auto-Management-Raspberry-pi'))
 
 print(subprocess.getstatusoutput('git clone https://github.com/aafdarweesh/Auto-Management-Raspberry-pi'))
 
-time.sleep(50) #sleep for 1 min
+time.sleep(5) #sleep for 1 min
 
 try :
-	exec(open("Auto-Management-Raspberry-pi/UpdateRepository.py").read())#Update Repository
+	#exec(open("Auto-Management-Raspberry-pi/UpdateRepository.py").read())#Update Repository
 	exec(open("Auto-Management-Raspberry-pi/RunProgram.py").read())#Run python file in the Repository after updating
 finally :
 	exit()
