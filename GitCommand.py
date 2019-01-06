11715b2d1851c6bb259b05e94e438a870702d5b4
aafdarweesh <aafdarweesh@gmail.com>
2019-01-06 10:14:06 +0200

    Update GitCommand.py

--- GitCommand.py
+++ GitCommand.py
@@ -17,7 +17,7 @@ def check_internet():
 
 
 if check_internet() == True:
-	print(subprocess.getstatusoutput('pkill -f Auto-Management-Raspberry-pi/Streaming.py'))#Stop the execution of the current program
+	print(subprocess.getstatusoutput('pkill -f Auto-Management-Raspberry-pi/RaspberryPiInterface.py'))#Stop the execution of the current program
 	#remove the current version of it
 	print(subprocess.getstatusoutput('rm Auto-Management-Raspberry-pi/*'))
 	print(subprocess.getstatusoutput('rm -rf Auto-Management-Raspberry-pi'))
@@ -28,7 +28,7 @@ if check_internet() == True:
 
 	try :
 		#os.system('python3 Auto-Management-Raspberry-pi/RunProgram.py')
-		os.system('python3 Auto-Management-Raspberry-pi/Streaming.py')
+		os.system('python3 Auto-Management-Raspberry-pi/RaspberryPiInterface.py')
 		#exec(open("Auto-Management-Raspberry-pi/UpdateRepository.py").read())#Update Repository
 		#exec(open("Auto-Management-Raspberry-pi/RunProgram.py").read())#Run python file in the Repository after updating
 	finally :
